ea28eb207c9d02074cdff8840efe404bbcb51bcc
Sean Chen <sean820816@yahoo.com.tw>
2016-10-31 00:48:39 +0800

    Initial Commit

---
+++ HackNTU-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackNTU-Demo
+//
+//  Created by SEAN HD on 2016/10/31.
+//  Copyright © 2016年 Appar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
